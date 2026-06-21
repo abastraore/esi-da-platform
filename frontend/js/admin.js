23dedf06bddf21917a48e5feb6e98244aefead56
Ch3icklandry <cheicksome01@gmail.com>
2026-06-21 14:50:39 +0000

    Update admin.js

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -1,9 +1,8 @@
-// admin.js — page de connexion
-// La logique est dans auth.js, ce fichier gère l'UI de la page login
+
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Si déjà connecté, rediriger directement
-  if (sessionStorage.getItem('esi_token')) {
+
+  /*if (sessionStorage.getItem('esi_token')) {
     window.location.href = 'dashboard.html';
-  }
+  }*/
 });
